ffcb85094803850eb5ef8540c615f6f239b91202
viviana <julissa1999estrella@gmail.com>
2023-05-12 16:57:16 -0500

    feat: first commit with rest

---
+++ src/main/java/com/vjem/exercisedevops/ExerciseDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.vjem.exercisedevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vjem/exercisedevops/ExerciseDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vjem.exercisedevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
